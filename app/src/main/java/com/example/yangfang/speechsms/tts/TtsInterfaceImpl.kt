4f7e85d55786628e82de632d7b14a5f689d3c250
yangfang <yf19911203@gmail.com>
2018-06-27 15:07:34 +0800

    修改bug

--- app/src/main/java/com/example/yangfang/speechsms/tts/TtsInterfaceImpl.kt
+++ app/src/main/java/com/example/yangfang/speechsms/tts/TtsInterfaceImpl.kt
@@ -16,7 +16,7 @@ class TtsInterfaceImpl(val source: TtsSource) : TtsInterface, SynthesizerListene
     // 引擎类型
     private val mEngineType = SpeechConstant.TYPE_CLOUD
 
-    private lateinit var mTts: SpeechSynthesizer
+    private var mTts: SpeechSynthesizer? = null
     var speechText = ""
 
     override fun set(text: String): TtsInterface {
@@ -24,24 +24,36 @@ class TtsInterfaceImpl(val source: TtsSource) : TtsInterface, SynthesizerListene
         return this
     }
 
+
+    override fun speech() {
+        if (speechText.isNotEmpty()) {
+            setParam()
+            val code = mTts!!.startSpeaking(speechText, this)
+            Log.e("TtsInterfaceImpl", "$code")
+            if (code != ErrorCode.SUCCESS) {
+                Log.e("TtsInterfaceImpl", "语音合成错误码$code")
+            }
+        }
+    }
+
     private fun setParam() {
         // 清空参数
-        mTts.setParameter(SpeechConstant.PARAMS, null)
+        mTts!!.setParameter(SpeechConstant.PARAMS, null)
         if (mEngineType.equals(SpeechConstant.TYPE_CLOUD)) {
             // 根据合成引擎设置相应参数
-            mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD)
+            mTts!!.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD)
             // 设置在线合成发音人
-            mTts.setParameter(SpeechConstant.VOICE_NAME, voicer)
+            mTts!!.setParameter(SpeechConstant.VOICE_NAME, voicer)
             //设置合成语速
-            mTts.setParameter(SpeechConstant.SPEED, "50")
+            mTts!!.setParameter(SpeechConstant.SPEED, "50")
             //设置合成音调
-            mTts.setParameter(SpeechConstant.PITCH, "50")
+            mTts!!.setParameter(SpeechConstant.PITCH, "50")
             //设置合成音量
-            mTts.setParameter(SpeechConstant.VOLUME, "50")
+            mTts!!.setParameter(SpeechConstant.VOLUME, "50")
         } else {
-            mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_LOCAL)
+            mTts!!.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_LOCAL)
             // 设置本地合成发音人 voicer为空，默认通过语记界面指定发音人。
-            mTts.setParameter(SpeechConstant.VOICE_NAME, "")
+            mTts!!.setParameter(SpeechConstant.VOICE_NAME, "")
             /**
              * TODO 本地合成不设置语速、音调、音量，默认使用语记设置
              * 开发者如需自定义参数，请参考在线合成参数设置
@@ -49,15 +61,15 @@ class TtsInterfaceImpl(val source: TtsSource) : TtsInterface, SynthesizerListene
 
         }
         //设置播放器音频流类型
-        mTts.setParameter(SpeechConstant.STREAM_TYPE, "3")
+        mTts!!.setParameter(SpeechConstant.STREAM_TYPE, "3")
         // 设置播放合成音频打断音乐播放，默认为true
-        mTts.setParameter(SpeechConstant.KEY_REQUEST_FOCUS, "true")
+        mTts!!.setParameter(SpeechConstant.KEY_REQUEST_FOCUS, "true")
 
         // 设置音频保存路径，保存音频格式支持pcm、wav，设置路径为sd卡请注意WRITE_EXTERNAL_STORAGE权限
         // 注：AUDIO_FORMAT参数语记需要更新版本才能生效
-        mTts.setParameter(SpeechConstant.AUDIO_FORMAT, "wav")
+        mTts!!.setParameter(SpeechConstant.AUDIO_FORMAT, "wav")
         val ts = Environment.getExternalStorageDirectory().absolutePath + "/msc/tts.wav"
-        mTts.setParameter(SpeechConstant.TTS_AUDIO_PATH, ts)
+        mTts!!.setParameter(SpeechConstant.TTS_AUDIO_PATH, ts)
     }
 
     override fun create(): TtsInterface {
@@ -77,9 +89,11 @@ class TtsInterfaceImpl(val source: TtsSource) : TtsInterface, SynthesizerListene
     }
 
     override fun destroy() {
-        mTts.stopSpeaking()
-        // 退出时释放连接
-        mTts.destroy()
+        if (mTts != null) {
+            mTts!!.stopSpeaking()
+            // 退出时释放连接
+            mTts!!.destroy()
+        }
     }
 
     override fun onCompleted(error: SpeechError?) {
@@ -110,17 +124,6 @@ class TtsInterfaceImpl(val source: TtsSource) : TtsInterface, SynthesizerListene
     }
 
     override fun onSpeakResumed() {
-    }
-
-    override fun speech() {
-        if (speechText.isNotEmpty()) {
-            setParam()
-            val code = mTts.startSpeaking(speechText, this)
-            Log.e("TtsInterfaceImpl", "$code")
-            if (code != ErrorCode.SUCCESS) {
-                Log.e("TtsInterfaceImpl", "语音合成错误码$code")
-            }
-        }
     }
 
 
